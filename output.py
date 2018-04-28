9c655bdd5089484cc3128a6ed2573a157f12d8eb
Yijin <yijinkang@gmail.com>
2018-04-28 20:32:38 +0200

    use MerchItem objects for outputing

--- output.py
+++ output.py
@@ -8,35 +8,45 @@ class MerchItem:
         self.name = name
         self.cost = cost
         self.quantity = quantity
-    def get_picture(self):
-        Image.open(self.image_key, 'r')
+    def get_icon(self):
+        return Image.open(self.image_key, 'r')
 
-x_img = 20
-x_cost = 300
+x_icon = 20
+x_cost = 400
 y_space = 25
-fnt = ImageFont.truetype('./fonts/roboto/Roboto-Regular.ttf', 24)
+title_fnt = ImageFont.truetype('./fonts/roboto/Roboto-Regular.ttf', 20)
+fnt = ImageFont.truetype('./fonts/roboto/Roboto-Regular.ttf', 15)
+
+def write_title(draw, coord, text):
+    draw.text(coord, text, font=title_fnt, fill=(0,0,0))
+
+def write(draw, coord, text):
+    draw.text(coord, text, font=fnt, fill=(0,0,0))
 
 def image(items):
-    """creates an image named res_img.png that displays items with icons and costs
-    items = a list of tuples (image_name, item_name, item_cost)"""
+    """creates an image named res_img.png that displays items with icons and costs"""
+
+    # pre-compute icon sizes to get the overall image formatted nicely
+    icons = [ item.get_icon() for item in items ]
+    max_icon_width = 0
+    max_icon_height = 0
+    for icon in icons:
+        icon_w, icon_h = icon.size
+        max_icon_width = max(max_icon_width, icon_w)
+        max_icon_height = max(max_icon_height, icon_h)
+    x_name = 2 * x_icon + max_icon_width
     
-    imgs = [ Image.open(item[0], 'r') for item in items ]
-    max_img_width = 0
-    max_img_height = 0
-    for img in imgs:
-        img_w, img_h = img.size
-        max_img_width = max(max_img_width, img_w)
-        max_img_height = max(max_img_height, img_h)
-    
-    img_height = len(items) * (y_space + max_img_height) + y_space
+    img_height = (len(items) + 1) * (y_space + max_icon_height) + y_space
     img = Image.new('RGB', (500, img_height), color = 'white')
     d = ImageDraw.Draw(img)
     y = y_space
+    write_title(d, (x_icon, y), "Item")
+    write_title(d, (x_cost, y), "Cost")
     for i in range(0,len(items)):
-        img.paste(imgs[i], (x_img, y))
-        d.text((2 * x_img + max_img_width, y), items[i][1], font=fnt, fill=(0,0,0))
-        d.text((x_cost, y), str(items[i][2]), font=fnt, fill=(0,0,0))
-        y += y_space + max_img_height
+        y += y_space + max_icon_height
+        img.paste(icons[i], (x_icon, y))
+        write(d, (x_name, y), items[i].name)
+        write(d, (x_cost, y), items[i].cost)
     img.save('res_img.png')
 
 ### below this is for testing purposes
@@ -47,7 +57,7 @@ test_sizes = [ (32, 25), (30, 32) ]
 def test(items):
     for i in range(0, len(items)):
         img = Image.new('RGB', test_sizes[i % 2], color = test_colors[i % 3])
-        img.save(items[i][0])
+        img.save(items[i].image_key)
     image(items)
 
-# test([ ('img' + str(x) + '.png', 'Item ' + str(x), 2 * x) for x in range(0, 12) ])
+# test([ MerchItem('img' + str(x) + '.png', 'Item ' + str(x), str(1000000 * x), str(1000000 * x)) for x in range(0, 12) ])
