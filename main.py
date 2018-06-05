098115966f4ace161cc147eeac4181658a603880
Colton Sowers <cdsowers@asu.edu>
2018-06-04 19:40:05 -0700

    AH server tags notifications

--- main.py
+++ main.py
@@ -79,8 +79,11 @@ async def auto_user_notifs(item):
 async def ah_test():
     items = request.parse_merch_items()
     data = userdb.ah_roles(items)
+    print(data)
     roles = [role_tuple[0].strip() for role_tuple in data]
+    print(roles)
     b = [role + '\n' for role in roles]
+    print(b)
     tag_string = "Tags: " + ''.join(b)
     print(tag_string)
     await bot.send_message(discord.Object(id=config.ah_chat_id), tag_string)
