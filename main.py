6db16eb35f8baea2888e04ae4bac1d8576c08b23
Colton Sowers <cdsowers@asu.edu>
2018-06-05 00:05:42 -0700

    added test function to notify for all items

--- main.py
+++ main.py
@@ -99,6 +99,16 @@ async def user_notifs(*, item):
         await bot.send_message(member, "{0} is in stock!".format(item))
         print(user)
 
+@bot.command()
+async def notif_test():
+    items = [item.name for item in request.parse_merch_items()]
+    for item in items:
+        data = userdb.users(item)
+        users = [user_tuple[0].strip() for user_tuple in data]
+        for user in users:
+            member = bot.get_server(userdb.user_server(user)).get_member(user_id=user)
+            await bot.send_message(member, "{0} is in stock!".format(item))
+
 @bot.command(pass_context=True, name='merch', aliases=['merchant', 'shop', 'stock'])
 async def merchant(ctx):
     """Displays the daily Traveling merchant stock."""
