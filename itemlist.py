ce6eec79440390c428b03e520b000cb1e08ade7f
Colton Sowers <cdsowers@asu.edu>
2018-06-05 19:39:59 -0700

    verification validity of new notifications

--- itemlist.py
+++ itemlist.py
@@ -1,29 +1,29 @@
-item_list = ['Small goebie burial charm'
-             'Goebie burial charm'
-             'Menaphite gift offering (small)'
-             'Menaphite gift offering (medium)'
-             'Unstable air rune'
-             'Anima crystal'
-             'Slayer VIP Coupon'
-             'D&D token (daily)'
-             'Unfocused damage enhancer'
-             'Sacred clay'
-             'Shattered anima'
-             'Advanced pulse core'
-             'Livid plant'
-             'Gift for the Reaper'
-             'Silverhawk down'
-             'Large goebie burial charm'
-             'Message in a bottle'
-             'Dragonkin lamp'
-             'Dungeoneering Wildcard'
-             'Menaphite gift offering (large)'
-             'Taijitu'
-             'D&D token (weekly)'
-             'D&D token (monthly)'
-             'Starved ancient effigy'
-             'Harmonic dust'
-             'Crystal triskelion'
-             'Deathtouched dart'
+item_list = ['Small goebie burial charm',
+             'Goebie burial charm',
+             'Menaphite gift offering (small)',
+             'Menaphite gift offering (medium)',
+             'Unstable air rune',
+             'Anima crystal',
+             'Slayer VIP Coupon',
+             'D&D token (daily)',
+             'Unfocused damage enhancer',
+             'Sacred clay',
+             'Shattered anima',
+             'Advanced pulse core',
+             'Livid plant',
+             'Gift for the Reaper',
+             'Silverhawk down',
+             'Large goebie burial charm',
+             'Message in a bottle',
+             'Dragonkin lamp',
+             'Dungeoneering Wildcard',
+             'Menaphite gift offering (large)',
+             'Taijitu',
+             'D&D token (weekly)',
+             'D&D token (monthly)',
+             'Starved ancient effigy',
+             'Harmonic dust',
+             'Crystal triskelion',
+             'Deathtouched dart',
              'Unfocused reward enhancer']
 
--- main.py
+++ main.py
@@ -133,8 +133,12 @@ async def merchant(ctx):
 async def addnotif(ctx, *, item):
     """Adds an item to a user's notify list."""
     stritem = str(item).lower()
-    if not stritem.replace(' ', '').isalnum():
-        await bot.say("lul")
+    lst = [item.lower() for item in itemlist.item_list]
+    if stritem not in lst:
+        await bot.say("Make sure you're spelling your item correctly!\nCheck your PMs for a list of correct spellings, or refer to the wikia page.")
+        b = [item + '\n' for item in itemlist.item_list]
+        itemstrv2 = 'Possible items:\n'.join(b)
+        await bot.send_message(ctx.message.author, itemstrv2)
         return
     if not userdb.pref_exists(ctx.message.author.id, stritem):
         userdb.new_pref(ctx.message.author.id, ctx.message.author, stritem, ctx.message.server.id)
