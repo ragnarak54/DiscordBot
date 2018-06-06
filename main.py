3bb403f01053fbbdfe4450312f24d958b89f424e
Colton Sowers <cdsowers@asu.edu>
2018-06-05 18:39:19 -0700

    verification validity of new notifications

--- main.py
+++ main.py
@@ -134,8 +134,9 @@ async def addnotif(ctx, *, item):
     """Adds an item to a user's notify list."""
     stritem = str(item).lower()
     if not str(item) in itemlist.item_list:
-        await bot.say("Make sure you're spelling your item correctly!\n Check your PMs for a list of correct spellings, or refer to the wikia page.")
-        await bot.send_message(ctx.message.author, itemlist.item_list)
+        await bot.say("Make sure you're spelling your item correctly!\nCheck your PMs for a list of correct spellings, or refer to the wikia page.")
+        itemstr = "".join(itemlist.item_list + "\n")
+        await bot.send_message(ctx.message.author, itemstr)
         return
     if not userdb.pref_exists(ctx.message.author.id, stritem):
         userdb.new_pref(ctx.message.author.id, ctx.message.author, stritem, ctx.message.server.id)
