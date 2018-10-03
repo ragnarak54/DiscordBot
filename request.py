63a925e5ad12704149cf620c9205fa8432dd7853
Colton Sowers <cdsowers@asu.edu>
2018-10-02 20:47:17 -0700

    migrate to new wiki page

--- request.py
+++ request.py
@@ -58,7 +58,7 @@ class MerchWebsiteParser(HTMLParser):
 
 def parse_merch_items():
     parser = MerchWebsiteParser()
-    r = requests.get('http://runescape.wikia.com/wiki/Travelling_Merchant%27s_Shop')
+    r = requests.get('https://runescape.wiki/w/Travelling_Merchant%27s_Shop')
     parser.feed(r.text)
     return parser.merch_items
 
@@ -87,6 +87,6 @@ class DateParser(HTMLParser):
 
 def parse_stock_date():
     parser = DateParser()
-    r = requests.get('http://runescape.wikia.com/wiki/Travelling_Merchant%27s_Shop')
+    r = requests.get('https://runescape.wiki/w/Travelling_Merchant%27s_Shop')
     parser.feed(r.text)
     return parser.date[:2].strip()
