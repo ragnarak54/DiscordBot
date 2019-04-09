3b07047856360dfcf1e018622db748ee9bff6538
ragnarak54 <colpolmen@gmail.com>
2019-04-09 13:26:00 -0700

    refactor to rewrite code, add setup method for future extension development

--- error_handler.py
+++ error_handler.py
@@ -4,11 +4,12 @@ from discord.ext import commands
 import discord
 
 
-class CommandErrorHandler:
+class CommandErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_command_error(self, error: Exception, ctx: commands.Context):
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: Exception):
         """The event triggered when an error is raised while invoking a command."""
 
         if hasattr(ctx.command, 'on_error'):
@@ -21,20 +22,21 @@ class CommandErrorHandler:
             return
 
         elif isinstance(error, commands.DisabledCommand):
-            await self.bot.send_message(ctx.message.channel, '{} has been disabled.'.format(ctx.command))
-            return
+            return await ctx.send(f'{ctx.command} has been disabled.')
 
         elif isinstance(error, commands.NoPrivateMessage):
             try:
-                await self.bot.send_message(ctx.message.author, '{} can not be used in Private Messages.'.format(ctx.command))
-                return
+                return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
             except discord.Forbidden:
                 pass
 
         elif isinstance(error, commands.BadArgument):
             if ctx.command.qualified_name == 'tag list':
-                await self.bot.send_message(ctx.message.channel, 'I could not find that member. Please try again.')
-                return
+                return await ctx.send('I could not find that member. Please try again.')
 
         print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+
+def setup(bot):
+    bot.add_cog(CommandErrorHandler(bot))
